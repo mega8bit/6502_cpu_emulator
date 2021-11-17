ac00f44bc0c6e8376012a94318a9d9917e8c183c
Rodionov <rodionov-andrey@ro.ru>
2021-11-17 21:47:08 +0300

    Hello world example asm file

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -1,6 +1,20 @@
 .SEGMENT "RESET"
 .WORD $8000
 
+
 .SEGMENT "CODE"
-LDA #01
+LDY $00
+LDA Message, Y
+
+Loop:
+STA $2000
+INY
+LDA Message, Y
+CMP $0A
+BNE Loop
+
+STA $2000
 NOP
+
+Message:
+.BYTE "Hello World!", $0A
